45a178dae3e0d2492af17eec32cb545404370225
Tautvydas <95254346+zarta21@users.noreply.github.com>
2024-01-15 12:40:24 +0100

    Add files via upload

---
+++ front-end/src/App.js
@@ -0,0 +1,52 @@
+import './app.scss';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import Announcement from './components/Announcement/Announcement';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+import Products from './pages/Products/Products';
+
+const Layout = () => {
+  return (
+    <div className='App'>
+      <Announcement />
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div> 
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/products/:id',
+        element: <Products />
+      },
+      {
+        path: '/product/:id',
+        element: <Product />
+      }
+    ]
+  }
+])
+
+
+function App() {
+  return (
+    <div >
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
