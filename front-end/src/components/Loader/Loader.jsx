f969c8be765419ce009d6a4783823ba9f0bd1f86
Tautvydas <95254346+zarta21@users.noreply.github.com>
2024-01-22 00:01:57 +0100

    Create Loader.jsx

---
+++ front-end/src/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './loader.scss'
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+        <div className="wrapper">
+            <span></span>
+        </div>
+    </div>
+  )
+}
+
+export default Loader
